cb4e963d999bdad6924f9960301048b7d0007471
Rito <ritaprava95@gmail.com>
2018-12-23 19:05:06 +0530

    Update Digit_Recognizer.py

--- Digit_Recognizer.py
+++ Digit_Recognizer.py
@@ -1,13 +1,9 @@
-# Digit_Recognizer
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 18 23:31:08 2018
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+@author: rito
+"""
 
 import os
 print(os.listdir("../input"))
